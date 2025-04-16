122d9bb5e3b3d5f5c504783fc9738e5686937391
jaipearl27 <jaix2793@gmail.com>
2025-04-16 12:11:46 +0530

    removed unnecesssary code

--- Backend/src/routes/freeHeadshotRoute.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { upload } from "../utils/multer.js";
-import { freeHeadshotCheckout } from "../controller/freeHeadshotController.js";
-
-const freeHeadshotRouter = express.Router();
-freeHeadshotRouter.route("/").post(upload.array("images"),freeHeadshotCheckout);
-
-export default freeHeadshotRouter;
